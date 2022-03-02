9fbb1416defcdb745b5d249d19149743dc5b3e7f
ruthdgalvan <ruthdgalvan@gmail.com>
2022-03-01 21:27:03 -0500

    US9_TestCompleted2

--- src/test/java/com/nextbasecrm/tests/US9AccessCorrectModules_Ruth.java
+++ src/test/java/com/nextbasecrm/tests/US9AccessCorrectModules_Ruth.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -50,7 +51,7 @@ public class US9AccessCorrectModules_Ruth {
             String actualTitle=driver.getTitle();
             Assert.assertEquals(actualTitle, expectedTitle);
 
-
+            driver.close();
 
         }
     }
